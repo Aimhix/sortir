c2c5749d4fb66481f667c2090d8e0c1298ed2308
mthomas2023 <nyak1937@gmail.com>
2023-11-14 15:37:00 +0100

    Update fixtures hash password (real one)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,6 +10,7 @@ use App\Entity\Status;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
