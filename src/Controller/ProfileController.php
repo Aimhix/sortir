e58f8f2346c00dc769660b1a348d17fae7984df4
QLebeau <lebeau.quentin@yahoo.fr>
2023-11-14 14:16:42 +0100

    ProfileController.php ProfileType.php / twig associés (show and edit)

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\EditProfileType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfileController extends AbstractController
+{
+    #[Route('/profile', name: 'app_profile')]
+    public function showProfile(): Response
+    {
+        //Récupérer user
+        $user = $this->getUser();
+
+        //Vérifier si user est connecté
+        if (!$user) {
+            //On redirige vers la page de connexion
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('profile/showProfile.html.twig');
+    }
+
+    #[Route('/editprofile', name: 'app_editprofile')]
+    public function edit(Request $request): Response
+    {
+
+        $user = $this->getUser();
+
+        // Formulaire ProfileType
+        $form = $this->createForm(EditProfileType::class, $user);
+        // Soumission formulaire
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Vérifier pseudo
+            $pseudo = $form->get('pseudo')->getData();
+            $existingUser = $this->getDoctrine()->getRepository(User::class)->findOneBy(['pseudo' => $pseudo]);
+
+            if ($existingUser && $existingUser->getId() !== $user->getId()) {
+                $this->addFlash('error', 'Le pseudo est déjà pris');
+
+                return $this->redirectToRoute('app_profile_editer');
+            }
+            //Enregistrer les modifications dans la BDD
+            $profileManager = $this->getDoctrine()->getManager();
+            $profileManager->flush();
+
+            $this->addFlash('success', 'Profil mis à jour');
+
+            return $this->redirectToRoute('app_profile');
+
+        }
+
+        return $this->render('profile/editProfile.html.twig',
+            ['form' => $form->createView(),]);
+    }
+
+}
---
+++ src/Form/ProfileType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('pseudo', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('nom', TextType::class)
+            ->add('telephone', TelType::class)
+            ->add('email', EmailType::class)
+            ->add('ville');
+
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'App\Entity\User',
+        ]);
+    }
+}
