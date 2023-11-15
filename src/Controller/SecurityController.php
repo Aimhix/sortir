28169fe406179051be5d54e9bccfb6a71e20e592
QLebeau <lebeau.quentin@yahoo.fr>
2023-11-15 12:17:59 +0100

    Connexion ok

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -19,11 +19,9 @@ class SecurityController extends AbstractController
 
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();  // fait appel méthode dans authentification
-       // return $this->redirectToRoute('app_activity_index');
-       return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        // return $this->redirectToRoute('app_activity_index');
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
-
-
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): RedirectResponse
