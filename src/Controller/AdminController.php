ee8fe09c1f8378cc8a0e71a2585afe5ab8c25c33
cassis53 <cassandra.bru2023@campus-eni.fr>
2023-11-17 09:38:36 +0100

    modit admin manuelle header et new_user

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin', name: 'app_admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+
+    #[Route('/admin/new-user', name: 'admin_new_user')]
+    public function newUser(Request $request): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserRegistrationFormType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Sauvegarder le nouvel utilisateur dans la base de données
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // Redirection vers une autre page après la création réussie de l'utilisateur
+            return $this->redirectToRoute(' '); // route à définir
+        }
+
+        return $this->render('admin/new_user.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+
+}
