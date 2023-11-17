82c8b90ab0ec6b867f1142a98938819d17037654
cassis53 <cassandra.bru2023@campus-eni.fr>
2023-11-17 10:14:45 +0100

    admin pas fini

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserRegistrationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
